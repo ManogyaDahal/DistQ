432a4a7f6758670f2e2229fb5cad2ce65010cc25
Homrajshahi <bindashahi473@gmail.com>
2026-06-25 01:05:17 +0545

    changes made and test-sdk

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,10 +16,6 @@ func main() {
 	mux.HandleFunc("GET /tasks/{id}", handler.GetStatus)
 	mux.HandleFunc("GET /tasks", handler.ListPending)
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(`{"message":"DistQ API is running","endpoints":["POST /tasks","GET /tasks/:id","GET /tasks"]}`))
-	})
 	log.Println("DistQ API running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
